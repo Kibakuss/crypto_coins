334a4ecc291df5aa6b7b0e4d20fb4594bb3f57ec
kibakuss <duxabank@gmail.com>
2023-04-16 16:36:43 +0500

    add refresh indicator and equtable

--- lib/features/crypto_list/bloc/crypto_list_bloc.dart
+++ lib/features/crypto_list/bloc/crypto_list_bloc.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:cripto_coins/repositories/crypto_coins/abstract_coins_repository.dart';
 import 'package:cripto_coins/repositories/crypto_coins/models/crypto_coin.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'crypto_list_event.dart';
@@ -9,10 +12,16 @@ class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
   CryptoListBloc(this.coinsRepository) : super(CryptoListInitial()) {
     on<LoadCryptoList>((event, emit) async {
       try {
+        if (state is! CryptoListLoaded) {
+          emit(CryptoListLoading());
+        }
+
         final coinsList = await coinsRepository.getCoinsList();
         emit(CryptoListLoaded(coinsList: coinsList));
       } catch (e) {
         emit(CryptoListLoadingFailure(exception: e));
+      } finally {
+        event.completer?.complete();
       }
 
       //
--- lib/features/crypto_list/view/crypto_list_screen.dart
+++ lib/features/crypto_list/view/crypto_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cripto_coins/features/crypto_list/bloc/crypto_list_bloc.dart';
 import 'package:cripto_coins/features/crypto_list/widgets/widgets.dart';
 import 'package:cripto_coins/repositories/crypto_coins/abstract_coins_repository.dart';
@@ -28,29 +30,47 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
         appBar: AppBar(
           title: const Text('CryptoCurrenciesList'),
         ),
-        body: BlocBuilder<CryptoListBloc, CryptoListState>(
-          bloc: _cryptoListBloc,
-          builder: (context, state) {
-            if (state is CryptoListLoaded) {
-              return ListView.separated(
-                padding: const EdgeInsets.only(top: 16),
-                itemCount: state.coinsList.length,
-                separatorBuilder: (context, index) => const Divider(),
-                itemBuilder: (context, i) {
-                  final coin = state.coinsList[i];
-
-                  return CryotoCoinTile(coin: coin);
-                },
-              );
-            }
-            if (state is CryptoListLoadingFailure) {
-              return const Center(
-                child: Text('Something went wrong'),
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
+        body: RefreshIndicator(
+          onRefresh: () async {
+            final completer = Completer();
+            _cryptoListBloc.add(LoadCryptoList(completer: completer));
+            return completer.future;
           },
+          child: BlocBuilder<CryptoListBloc, CryptoListState>(
+            bloc: _cryptoListBloc,
+            builder: (context, state) {
+              if (state is CryptoListLoaded) {
+                return ListView.separated(
+                  padding: const EdgeInsets.only(top: 16),
+                  itemCount: state.coinsList.length,
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, i) {
+                    final coin = state.coinsList[i];
+
+                    return CryotoCoinTile(coin: coin);
+                  },
+                );
+              }
+              if (state is CryptoListLoadingFailure) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Something went wrong'),
+                      const SizedBox(height: 20),
+                      TextButton(
+                          onPressed: () {
+                            _cryptoListBloc.add(LoadCryptoList());
+                          },
+                          child: const Text('Try again'))
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ));
   }
 }
